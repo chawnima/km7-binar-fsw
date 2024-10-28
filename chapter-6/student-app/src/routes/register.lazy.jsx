86b9d8d11d2cfbc1212b2797281e644dddf5de8e
chawnima <mferdyalviansyah@gmail.com>
2024-10-28 14:27:57 +0700

    add register

--- chapter-6/student-app/src/routes/register.lazy.jsx
+++ chapter-6/student-app/src/routes/register.lazy.jsx
@@ -11,16 +11,40 @@ export const Route = createLazyFileRoute("/register")({
 });
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [profilePicture, setFile] = useState(undefined);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("profile_picture", profilePicture);
+
+    const res = await fetch("http://localhost:3000/auth/register", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    if (data.success) {
+      alert(data.message);
+      localStorage.setItem("token", data.data.token);
+      return;
+    }
+    alert(data.message);
+  };
+
   return (
-    <div
-      className="d-flex justify-content-center align-items-center row"
-      style={{ height: "80vh" }}
-    >
-      <Card className="col-6">
-        <Card.Body>
-          <Card.Text className="">
-            <Form>
+    <Row className="mt-5">
+      <Col className="offset-md-3">
+        <Card>
+          <Card.Header className="text-center">Register</Card.Header>
+          <Card.Body>
+            <Form onSubmit={onSubmit}>
               <Form.Group
                 as={Row}
                 className="mb-3"
@@ -30,10 +54,14 @@ function Register() {
                   Name
                 </Form.Label>
                 <Col sm="10">
-                  <Form.Control type="text" placeholder="Your name" />
+                  <Form.Control
+                    type="text"
+                    placeholder="Your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </Col>
               </Form.Group>
-
               <Form.Group
                 as={Row}
                 className="mb-3"
@@ -43,10 +71,14 @@ function Register() {
                   Email
                 </Form.Label>
                 <Col sm="10">
-                  <Form.Control type="email" placeholder="email@example.com" />
+                  <Form.Control
+                    type="email"
+                    placeholder="email@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </Col>
               </Form.Group>
-
               <Form.Group
                 as={Row}
                 className="mb-3"
@@ -68,7 +100,7 @@ function Register() {
               <Form.Group
                 as={Row}
                 className="mb-3"
-                controlId="formPlaintextPassword"
+                controlId="formPlaintextRePassword"
               >
                 <Form.Label column sm="2">
                   Confirm Password
@@ -77,31 +109,34 @@ function Register() {
                   <Form.Control
                     type="password"
                     placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={rePassword}
+                    onChange={(e) => setRePassword(e.target.value)}
                   />
                 </Col>
               </Form.Group>
 
-              <Form.Group
-                as={Row}
-                className="mb-3"
-                controlId="formPlaintextPassword"
-              >
+              <Form.Group as={Row} className="mb-3" controlId="formFile">
                 <Form.Label column sm="2">
                   Profile Picture
                 </Form.Label>
                 <Col sm="10">
-                  <Form.Control type="file" />
+                  <Form.Control
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    accept=".png,.jpg"
+                  />
                 </Col>
               </Form.Group>
+              <div className="d-grid gap-2">
+                <Button type="submit" variant="primary">
+                  Register
+                </Button>
+              </div>
             </Form>
-          </Card.Text>
-          <Button variant="primary" className="col-12">
-            Register
-          </Button>
-        </Card.Body>
-      </Card>
-    </div>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={3}></Col>
+    </Row>
   );
 }
